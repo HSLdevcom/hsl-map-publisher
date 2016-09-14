deb63492e18338ee5c279501ad9c8007fa7f7ba3
Sebastian <sebastian.hognabba@gmail.com>
2016-09-14 16:15:18 +0300

    disabled react/prop-type rule from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     rules: {
         "indent": ["error", 4],
         "quotes": ["error", "double", {"avoidEscape": true}],
+        "react/prop-types": 0,
         "react/jsx-indent": ["error", 4],
         "react/jsx-filename-extension": ["error", {"extensions": [".js"]}],
         "react/jsx-space-before-closing": ["error", "never"]
