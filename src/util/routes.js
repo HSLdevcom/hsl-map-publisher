1e010cd74f5bd2139f0aa992e255fb86278927a6
asilventoinen <antti.silventoinen@iki.fi>
2016-10-04 15:38:37 +0300

    Fix bug in route diagram

--- src/util/routes.js
+++ src/util/routes.js
@@ -9,12 +9,12 @@ function routesToPaths(stop, routes) {
     const paths = [];
 
     // Get stops after given stop
-    const stops = routes.map(({stops}) => {
+    const stopLists = routes.map(({stops}) => {
         const index = stops.map(({stopId}) => stopId).indexOf(stop.stopId);
         return stops.slice(index).map(item => ({...item, duration: item.duration - stops[index].duration}));
     });
 
-    stops.forEach(stop => addStops(paths, stop));
+    stopLists.forEach(stops => addStops(paths, stops));
     paths.forEach(path => addRoutes(path, routes));
 
     return (paths.length > 1) ? {subpaths: paths} : paths[0];
@@ -36,23 +36,23 @@ const addStops = (paths, stops) => {
 
         if(index) {
             const commonStops = path.stops.slice(0, index);
-            const remainingStops = path.stops.slice(index + 1);
+            const remainingPath = path.stops.slice(index);
 
-            if(remainingStops.length) {
+            if(remainingPath.length) {
                 if(!path.subpaths) {
-                    path.subpaths = [{stops: remainingStops}];
+                    path.subpaths = [{ stops: remainingPath }];
                 } else {
-                    path.subpaths = [{stops: remainingStops, subpaths: path.subpaths}];
+                    path.subpaths = [{stops: remainingPath, subpaths: path.subpaths}];
                 }
                 path.stops = commonStops;
             }
 
-            // Stops in route that don't belong to current path
-            const remainingRouteStops = stops.slice(index + 1);
+            // Stops that don't belong to current path
+            const remainingStops = stops.slice(index);
 
-            if(remainingRouteStops.length) {
+            if(remainingStops.length) {
                 if(!path.subpaths) path.subpaths = [];
-                addStops(path.subpaths, remainingRouteStops);
+                addStops(path.subpaths, remainingStops);
             }
 
             return;
@@ -81,15 +81,16 @@ function addRoutes(path, routes) {
  * Returns an index where stop list differs from path
  * @param {Object} path
  * @param {Array} stops
- * @returns {number} - Index
+ * @returns {number} - One-based index (i.e. number of common stops)
  */
 function findSplitIndex(path, stops) {
     for(let i=0;i<path.stops.length;i++) {
-        if(i >= stops.length || stops[i].stopId !== path.stops[i].stopId) {
-            return i;
+        if(i === stops.length - 1 || stops[i].stopId !== path.stops[i].stopId) {
+           return i;
         }
     }
-    return path.stops.length - 1;
+    // We need each destination as a separate subpath -> split outer paths ealier
+    return path.subpaths ? path.stops.length : path.stops.length - 1;
 }
 
 export {
