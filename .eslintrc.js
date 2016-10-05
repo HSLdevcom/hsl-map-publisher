81496f70a5b63c2ded454f17c5ced47975325d2f
asilventoinen <antti.silventoinen@iki.fi>
2016-10-05 12:21:02 +0300

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,11 @@ module.exports = {
     extends: "airbnb",
     rules: {
         "indent": ["error", 4],
+        "no-plusplus": ["off"],
         "quotes": ["error", "double", {"avoidEscape": true}],
-        "react/prop-types": 0,
+        "react/prop-types": ["off"],
         "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
         "react/jsx-filename-extension": ["error", {"extensions": [".js"]}],
         "react/jsx-space-before-closing": ["error", "never"]
     },
