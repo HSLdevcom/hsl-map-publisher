942708142e7f6c161c8bdaae0930455f357cb80b
asilventoinen <antti.silventoinen@iki.fi>
2016-11-01 16:06:37 +0200

    Adjust zoom level based on stop density

--- src/util/stopPoster.js
+++ src/util/stopPoster.js
@@ -1,51 +1,84 @@
 import viewportMercator from "viewport-mercator-project";
 import { fetchStop, fetchStops, fetchRoutes, fetchTimetable, fetchMap } from "util/api";
 
-function fetchStopsInViewport(viewport, stopToOmit) {
-    // FIXME: Fetch active stops groups with valid timetables instead of all stops
-    return fetchStops().then((stops) => {
-        const visibleStops = stops
-            .filter(({ stopId }) => stopId !== stopToOmit.stopId)
-            .filter(({ lon, lat }) => viewport.contains([lon, lat]));
+const MAX_STOPS = 8;
 
-        const promises = visibleStops.map(stop =>
-            fetchRoutes(stop.stopId).then((routes) => {
-                const [x, y] = viewport.project([stop.lon, stop.lat]);
-                return { ...stop, routes, x, y };
-            })
-        );
+const MAX_ZOOM = 18;
+const MIN_ZOOM = 14;
+const STEP_ZOOM = 0.1;
 
-        return Promise.all(promises);
+const MAP_WIDTH = 1500;
+const MAP_HEIGHT = 1200;
+
+const MINI_MAP_WIDTH = 450;
+const MINI_MAP_HEIGHT = 360;
+const MINI_MAP_ZOOM = 9;
+
+function createViewport(stop, zoom) {
+    return viewportMercator({
+        longitude: stop.lon,
+        latitude: stop.lat,
+        zoom: zoom,
+        width: MAP_WIDTH,
+        height: MAP_HEIGHT,
     });
 }
 
-function fetchMaps(stop) {
-    const mapOptions = {
-        center: [stop.lon, stop.lat],
-        width: 1500,
-        height: 1200,
-        scale: 1,
-        zoom: 15,
-    };
+function calculateStopsViewport(activeStop, stops) {
+    let zoom;
+    let viewport;
+    let visibleStops = stops;
 
-    const miniMapOptions = {
-        center: [stop.lon, stop.lat],
-        width: 450,
-        height: 360,
-        zoom: 9,
-    };
+    // Increase zoom level until only max number of stops visible
+    for (zoom = MIN_ZOOM; zoom <= MAX_ZOOM; zoom += STEP_ZOOM) {
+        viewport = createViewport(activeStop, zoom);
+        visibleStops = visibleStops
+            .filter(({ stopId }) => stopId !== activeStop.stopId)
+            .filter(({ lon, lat }) => viewport.contains([lon, lat]));
+        if(visibleStops.length <= MAX_STOPS) break;
+    }
 
-    const viewport = viewportMercator({
-        longitude: mapOptions.center[0],
-        latitude: mapOptions.center[1],
-        zoom: mapOptions.zoom,
-        width: mapOptions.width,
-        height: mapOptions.height,
+    const projectedStops = visibleStops.map(stop => {
+        // Calculate pixel coordinates for each stops
+        const [x, y] = viewport.project([stop.lon, stop.lat]);
+        return { ...stop, x, y };
     });
 
-    return Promise
-        .all([fetchMap(mapOptions), fetchMap(miniMapOptions), fetchStopsInViewport(viewport, stop)])
-        .then(([map, miniMap, stops]) => ({ map, miniMap, mapOptions, miniMapOptions, stops }));
+    return { stops: projectedStops, viewport, zoom };
+}
+
+function fetchCompleteStops({ stops, viewport, zoom }) {
+    const promises = stops.map(
+        stop => fetchRoutes(stop.stopId).then((routes) => ({ ...stop, routes }))
+    );
+    return Promise.all(promises).then(stops => ({ stops, zoom }));
+}
+
+function fetchMaps(stop) {
+    // FIXME: Fetch active stops groups with valid timetables instead of all stops
+    return fetchStops()
+        .then(stops => calculateStopsViewport(stop, stops))
+        .then(result => fetchCompleteStops(result))
+        .then(({ stops, zoom }) => {
+            const mapOptions = {
+                center: [stop.lon, stop.lat],
+                width: MAP_WIDTH,
+                height: MAP_HEIGHT,
+                zoom: zoom,
+            };
+
+            const miniMapOptions = {
+                center: [stop.lon, stop.lat],
+                width: MINI_MAP_WIDTH,
+                height: MINI_MAP_HEIGHT,
+                zoom: MINI_MAP_ZOOM,
+            };
+
+            // Remove map options and use constants and stop lat lon in map component?
+            return Promise
+                .all([fetchMap(mapOptions), fetchMap(miniMapOptions)])
+                .then(([map, miniMap]) => ({map, miniMap, mapOptions, miniMapOptions, stops}));
+    });
 }
 
 /**
