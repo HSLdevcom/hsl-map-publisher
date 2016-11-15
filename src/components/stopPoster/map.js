ba22f14264a4295da301d04df535b5ea7fef5e05
asilventoinen <antti.silventoinen@iki.fi>
2016-11-15 18:02:35 +0200

    Show location on map

--- src/components/stopPoster/map.js
+++ src/components/stopPoster/map.js
@@ -8,16 +8,22 @@ import locationIcon from "icons/location.svg";
 
 import styles from "./map.css";
 
+// Max rows in label
 const MAX_LABEL_ROWS = 6;
 
-const Location = () => (
-    <div className={styles.location}>
+// Symbol size
+const STOP_RADIUS = 17;
+const LOCATION_RADIUS = 27;
+const LOCATION_RADIUS_MINI = 18;
+
+const Location = props => (
+    <div style={{ width: props.size, height: props.size }}>
         <img src={locationIcon} role="presentation"/>
     </div>
 );
 
 const Stop = props => (
-    <div className={styles.stop}>
+    <div style={{ width: props.size, height: props.size }}>
         <img src={getSymbol(props.stopId)} role="presentation"/>
     </div>
 );
@@ -54,8 +60,6 @@ const Map = (props) => {
     const mapStyle = { width: props.mapOptions.width, height: props.mapOptions.height };
     const miniMapStyle = { width: props.miniMapOptions.width, height: props.miniMapOptions.height };
 
-    // TODO: Add location <Location/>
-
     return (
         <div className={styles.root} style={mapStyle}>
             <div className={styles.container}>
@@ -65,10 +69,26 @@ const Map = (props) => {
             <div className={styles.overlays}>
                 <ItemContainer>
                     {props.stops.map((stop, index) => (
-                        <ItemWrapper key={index} x={stop.x} y={stop.y} angle={45} isFixed>
-                            <Stop {...stop}/>
+                        <ItemWrapper
+                            key={index}
+                            x={stop.x - STOP_RADIUS}
+                            y={stop.y - STOP_RADIUS}
+                            angle={45}
+                            isFixed
+                        >
+                            <Stop {...stop} size={STOP_RADIUS * 2}/>
                         </ItemWrapper>
                     ))}
+
+                    <ItemWrapper
+                        x={(mapStyle.width / 2) - LOCATION_RADIUS}
+                        y={(mapStyle.height / 2) - LOCATION_RADIUS}
+                        angle={45}
+                        isFixed
+                    >
+                        <Location size={LOCATION_RADIUS * 2}/>
+                    </ItemWrapper>
+
                     {props.stops.map((stop, index) => (
                         <ItemWrapper key={index} x={stop.x} y={stop.y} distance={15}>
                             <Label {...stop}/>
@@ -80,7 +100,9 @@ const Map = (props) => {
             <div className={styles.miniMap} style={miniMapStyle}>
                 <div className={styles.container}>
                     <img src={props.miniMap} role="presentation"/>
-
+                    <div className={styles.center} style={{ margin: -LOCATION_RADIUS_MINI }}>
+                        <Location size={LOCATION_RADIUS_MINI * 2}/>
+                    </div>
                 </div>
             </div>
         </div>
