022d03ec573f87f091141685d15e178429cac114
asilventoinen <antti.silventoinen@iki.fi>
2017-02-06 14:34:16 +0200

    Cleanup util methods

--- src/util/stopPoster.js
+++ src/util/stopPoster.js
@@ -24,22 +24,20 @@ function createViewport(stop, zoom) {
     });
 }
 
-function calculateStopsViewport(activeStop, stops) {
+function calculateStopsViewport(centeredStop, stops) {
     let zoom;
     let viewport;
-    let visibleStops = stops;
+    let visibleStops = stops.filter(({ stopId }) => stopId !== centeredStop.stopId);
 
     // Increase zoom level until only max number of stops visible
     for (zoom = MIN_ZOOM; zoom <= MAX_ZOOM; zoom += STEP_ZOOM) {
-        viewport = createViewport(activeStop, zoom);
-        visibleStops = visibleStops
-            .filter(({ stopId }) => stopId !== activeStop.stopId)
-            .filter(({ lon, lat }) => viewport.contains([lon, lat])); // eslint-disable-line
+        viewport = createViewport(centeredStop, zoom);
+        visibleStops = visibleStops.filter(({ lon, lat }) => viewport.contains([lon, lat])); // eslint-disable-line
         if (visibleStops.length <= MAX_STOPS) break;
     }
 
+    // Calculate pixel coordinates for each stop
     const projectedStops = visibleStops.map((stop) => {
-        // Calculate pixel coordinates for each stops
         const [x, y] = viewport.project([stop.lon, stop.lat]);
         return { ...stop, x, y };
     });
@@ -47,18 +45,18 @@ function calculateStopsViewport(activeStop, stops) {
     return { stops: projectedStops, zoom };
 }
 
-function fetchCompleteStops(options) {
+function fetchRoutesForStops(options) {
     const promises = options.stops.map(
         stop => fetchRoutes(stop.stopId).then(routes => ({ ...stop, routes }))
     );
-    return Promise.all(promises).then(stops => ({ stops, zoom: options.zoom }));
+    return Promise.all(promises).then(stops => ({ ...options, stops }));
 }
 
 function fetchMaps(stop) {
     // FIXME: Fetch active stops groups with valid timetables instead of all stops
     return fetchStops()
         .then(stops => calculateStopsViewport(stop, stops))
-        .then(result => fetchCompleteStops(result))
+        .then(options => fetchRoutesForStops(options))
         .then(({ stops, zoom }) => {
             const mapOptions = {
                 center: [stop.lon, stop.lat],
@@ -74,7 +72,6 @@ function fetchMaps(stop) {
                 zoom: MINI_MAP_ZOOM,
             };
 
-            // Remove map options and use constants and stop lat lon in map component?
             return Promise
                 .all([fetchMap(mapOptions), fetchMap(miniMapOptions)])
                 .then(([map, miniMap]) => ({ map, miniMap, mapOptions, miniMapOptions, stops }));
@@ -90,7 +87,7 @@ function fetchStopPosterProps(stopId) {
     return Promise
         .all([fetchStop(stopId), fetchTimetable(stopId), fetchRoutes(stopId)])
         .then(([stop, timetable, routes]) => (
-            fetchMaps(stop, routes).then(map => ({ map, stop, timetable, routes }))
+            fetchMaps(stop).then(map => ({ map, stop, timetable, routes }))
         ));
 }
 
