35e94bb8544570564428339ca7ca9e856c569c8c
asilventoinen <antti.silventoinen@iki.fi>
2017-02-10 16:14:59 +0200

    Update eslint rules. Enable react/prop-types.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,22 @@ module.exports = {
     rules: {
         "indent": ["error", 4],
         "no-plusplus": ["off"],
+        "no-restricted-syntax": ["off"],
+        "comma-dangle": ["error", {
+            "arrays": "always-multiline",
+            "objects": "always-multiline",
+            "imports": "always-multiline",
+            "exports": "always-multiline",
+            "functions": "never"
+        }],
         "quotes": ["error", "double", {"avoidEscape": true}],
-        "react/prop-types": ["off"],
         "react/jsx-indent": ["error", 4],
         "react/jsx-indent-props": ["error", 4],
         "react/jsx-filename-extension": ["error", {"extensions": [".js"]}],
-        "react/jsx-space-before-closing": ["error", "never"]
+        "react/jsx-tag-spacing": ["error", {"beforeSelfClosing": "never"}],
+        "react/jsx-space-before-closing": ["error", "never"],
+        "react/no-array-index-key": ["off"],
+        "jsx-a11y/img-has-alt": ["off"]
     },
     settings: {
         "import/resolver": "webpack"
