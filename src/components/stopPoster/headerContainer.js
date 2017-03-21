ced96bfed8323fe11432a205f7a1c459e1c6e858
Hannes Junnila <hannes.junnila@gmail.com>
2017-03-21 17:23:35 +0200

    refactor header container

--- src/components/stopPoster/headerContainer.js
+++ src/components/stopPoster/headerContainer.js
@@ -1,8 +1,10 @@
 import { gql, graphql } from "react-apollo";
+import mapProps from "recompose/mapProps";
+import apolloWrapper from "util/apolloWrapper";
 
 import Header from "./header";
 
-export default graphql(gql`
+const headerQuery = gql`
     query headerQuery($stopId: String!) {
         stop: stopByStopId(stopId: $stopId) {
             shortId
@@ -10,6 +12,10 @@ export default graphql(gql`
             nameSe
         }
     }
-`, {
-    props: props => props.data.stop,
-})(Header);
+`;
+
+const propsMapper = mapProps(props => props.data.stop);
+
+const HeaderContainer = apolloWrapper(propsMapper)(Header);
+
+export default graphql(headerQuery)(HeaderContainer);
