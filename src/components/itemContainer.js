b2105c740a26f9982d4c961c71918ea8167a17f6
Antti Silventoinen <antti.silventoinen@iki.fi>
2017-03-24 18:45:32 +0200

    Improve label placement speed

--- src/components/itemContainer.js
+++ src/components/itemContainer.js
@@ -1,9 +1,11 @@
 import React, { Component, PropTypes } from "react";
+
 import styles from "./itemContainer.css";
 
-const MAX_ITERATIONS = 10;
+const MAX_ITERATIONS = 50;
 const OVERLAP_COST_FIXED = 5;
-const DISTANCE_COST = 10;
+const DISTANCE_COST = 1;
+
 const MASK_MARGIN = 5;
 
 const angles = [-90, -40, -30, -20, -10, -5, -1, 1, 5, 10, 20, -30, 40, 90, 180];
@@ -82,29 +84,27 @@ Overlays.propTypes = {
  */
 class ItemContainer extends Component {
 
-    static getDistanceCost(positions) {
-        let sum = 0;
-        positions.forEach((position) => {
-            if (position.distance) sum += position.distance - position.initialDistance;
-        });
-        return sum * DISTANCE_COST;
-    }
-
+    /**
+     * Returns intersection area of two components
+     * @param {Object} a
+     * @param {Object} b
+     * @returns {number}
+     */
     static getIntersectionArea(a, b) {
         const width = Math.min(a.left + a.width, b.left + b.width) - Math.max(a.left, b.left);
         const height = Math.min(a.top + a.height, b.top + b.height) - Math.max(a.top, b.top);
         return Math.max(0, width) * Math.max(0, height);
     }
 
-    static getOverlappingComponents(positions, parentIndex) {
-        const matchedIndexes = [];
-        positions.forEach((position, index) => {
-            if (index !== parentIndex && !positions[index].isFixed &&
-                ItemContainer.getIntersectionArea(positions[index], positions[parentIndex]) > 0) {
-                matchedIndexes.push(index);
-            }
-        });
-        return matchedIndexes;
+    /**
+     * Returns cost for increased distances from anchor
+     * @param {Object[]} positions - Positions
+     * @param {number[]} indexes - Indexes to check
+     * @returns {number}
+     */
+    static getDistanceCost(positions, indexes) {
+        return DISTANCE_COST * indexes.reduce((prev, index) =>
+            prev + (positions[index].distance - positions[index].initialDistance), 0);
     }
 
     /**
@@ -116,16 +116,15 @@ class ItemContainer extends Component {
      * @returns {Object} - Updated position
      */
     static updatePosition(position, diff = {}) {
-        if (position.isFixed) {
-            return position;
-        }
+        if (position.isFixed) return position;
 
         let distance = "distance" in position ? position.distance : position.initialDistance;
         let angle = "angle" in position ? position.angle : position.initialAngle;
 
         if (diff.angle) angle = (angle + diff.angle) % 360;
         if (diff.distance) distance += diff.distance;
-        if (distance < position.distance) distance = position.distance;
+
+        if (distance < position.initialDistance) return null;
 
         const a = position.width / 2;
         const b = position.height / 2;
@@ -157,84 +156,108 @@ class ItemContainer extends Component {
         };
     }
 
-    static getUpdatedPositions(positions, indexToUpdate, diff) {
-        return positions.map((position, index) => {
-            if (index === indexToUpdate) {
-                return ItemContainer.updatePosition(position, diff);
-            }
-            return position;
-        });
-    }
-
     constructor(props) {
         super(props);
         this.state = {};
     }
 
     componentDidMount() {
+        this.updateBoundingBox();
         this.updateChildren();
     }
 
     componentDidUpdate(prevProps) {
         if (prevProps.children !== this.props.children) {
+            this.updateBoundingBox();
             this.updateChildren();
         }
     }
 
-    getCollisionCost(positions) {
-        let overflow = 0;
-        let overlap = 0;
+    getOverlappingComponent(positions, indexToOverlap) {
+        for (let i = 0; i < positions.length; i++) {
+            if (i !== indexToOverlap && !positions[i].isFixed &&
+                ItemContainer.getIntersectionArea(positions[i], positions[indexToOverlap]) > 0) {
+                return i;
+            }
+        }
+        return null;
+    }
 
-        const boundingBox = {
+    /**
+     * Returns cost for overlapping non-fixed components
+     * @param {Object[]} positions - Positions
+     * @param {number[]} indexes - Indexes to check
+     * @returns {number}
+     */
+    getOverlapCost(positions, indexes) {
+        let overlap = 0;
+        indexes.forEach((i) => {
+            for (let j = 0; j < positions.length; j++) {
+                if (!indexes.includes(j) || j > i) {
+                    const area = ItemContainer.getIntersectionArea(positions[i], positions[j]);
+                    const isFixed = positions[i].isFixed || positions[j].isFixed;
+                    overlap += (isFixed ? area * OVERLAP_COST_FIXED : area);
+                }
+            }
+        });
+        return overlap;
+    }
+
+    getCost(positions, indexes) {
+        const overlap = this.getOverlapCost(positions, indexes);
+        const distances = ItemContainer.getDistanceCost(positions, indexes);
+        return overlap + distances;
+    }
+
+    getPlacements(positions, indexToUpdate, updatedIndexes = []) {
+        const indexes = [...updatedIndexes, indexToUpdate];
+        const diffs = angles.reduce(
+            (prev, angle) => ([...prev, ...distances.map(distance => ({ angle, distance }))]),
+            []);
+        return diffs
+            .map((diff) => {
+                const updatedPosition = ItemContainer.updatePosition(positions[indexToUpdate], diff);
+                if (!updatedPosition || this.hasOverflow(updatedPosition)) return null;
+                return positions.map(
+                    (position, index) => ((index === indexToUpdate) ? updatedPosition : position)
+                );
+            })
+            .filter(value => !!value)
+            .map(value => ({ positions: value, indexes }));
+    }
+
+    getPlacementsForOverlapping(placements, indexToOverlap) {
+        return placements.reduce((prev, { positions, indexes }) => {
+            const index = this.getOverlappingComponent(positions, indexToOverlap);
+            if (!index) return prev;
+            return [...prev, ...this.getPlacements(positions, index, indexes)];
+        }, []);
+    }
+
+    comparePlacements(placement, other) {
+        // Initial placement, always return other
+        if (!placement.indexes) return other;
+
+        const indexes = [...new Set([...placement.indexes, ...other.indexes])];
+        const cost = this.getCost(placement.positions, indexes);
+        const costOther = this.getCost(other.positions, indexes);
+
+        return costOther < cost ? other : placement;
+    }
+
+    hasOverflow(position) {
+        const rectArea = position.width * position.height;
+        const overflow = rectArea - ItemContainer.getIntersectionArea(this.boundingBox, position);
+        return overflow > 0;
+    }
+
+    updateBoundingBox() {
+        this.boundingBox = {
             left: 0,
             top: 0,
             width: this.root.offsetWidth,
             height: this.root.offsetHeight,
         };
-
-        for (let i = 0; i < positions.length; i++) {
-            if (!positions[i].isFixed) {
-                const rectArea = positions[i].width * positions[i].height;
-                overflow += rectArea - ItemContainer.getIntersectionArea(boundingBox, positions[i]);
-            }
-            for (let j = i + 1; j < positions.length; j++) {
-                if (!positions[i].isFixed || !positions[j].isFixed) {
-                    const area = ItemContainer.getIntersectionArea(positions[i], positions[j]);
-                    const isFixed = positions[i].isFixed || positions[j].isFixed;
-                    overlap += isFixed ? area * OVERLAP_COST_FIXED : area;
-                }
-            }
-        }
-        return overlap + overflow;
-    }
-
-    getPlacement(positions) {
-        const collision = this.getCollisionCost(positions);
-        const distance = ItemContainer.getDistanceCost(positions);
-        const cost = collision + distance;
-        return { positions, cost };
-    }
-
-    getPlacements(positions, indexToMove) {
-        const diffs = angles.reduce(
-            (prev, angle) => ([...prev, ...distances.map(distance => ({ angle, distance }))]),
-        []);
-        return diffs.map((diff) => {
-            const updatedPositions = positions.map((position, index) => (
-                index === indexToMove ? ItemContainer.updatePosition(position, diff) : position
-            ));
-            return this.getPlacement(updatedPositions);
-        });
-    }
-
-    getPlacementsForOverlapping(placements, indexToOverlap) {
-        return placements
-            .map(({ positions }) => (
-                ItemContainer
-                    .getOverlappingComponents(positions, indexToOverlap)
-                    .map(index => this.getPlacements(positions, index))
-                    .reduce((prev, cur) => [...prev, ...cur], [])
-            )).reduce((prev, cur) => [...prev, ...cur], []);
     }
 
     updateChildren() {
@@ -245,10 +268,10 @@ class ItemContainer extends Component {
         const initialPositions = refs
             .map(ref => ref.getPosition())
             .map(position => ItemContainer.updatePosition(position));
-        let placement = this.getPlacement(initialPositions);
+        let placement = { positions: initialPositions };
 
         for (let iteration = 0; iteration < MAX_ITERATIONS; iteration++) {
-            if (placement.cost <= 0) break;
+            const previousPlacement = placement;
 
             placement.positions.forEach((position, index, positions) => { // eslint-disable-line
                 if (position.isFixed) return;
@@ -259,15 +282,14 @@ class ItemContainer extends Component {
                 // Get potential positions for components overlapping component at index
                 const placementForOverlapping = this.getPlacementsForOverlapping(placements, index);
 
-                const nextPlacement = [
+                placement = [
+                    placement,
                     ...placements,
                     ...placementForOverlapping,
-                ].reduce((prev, cur) => (cur.cost < prev.cost ? cur : prev));
-
-                if (nextPlacement.cost < placement.cost) {
-                    placement = nextPlacement;
-                }
+                ].reduce((prev, cur) => this.comparePlacements(prev, cur));
             });
+
+            if (placement === previousPlacement) break;
         }
 
         refs.forEach((ref, index) => {
