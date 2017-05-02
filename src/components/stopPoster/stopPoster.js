7053350fcf16d0b0468ea695e97609fdcbcfb668
Antti Silventoinen <antti.silventoinen@iki.fi>
2017-05-02 19:17:49 +0300

    Update stop poster layout if rendered content overflows

--- src/components/stopPoster/stopPoster.js
+++ src/components/stopPoster/stopPoster.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { JustifiedColumn, Spacer, FlexSpacer } from "components/util";
+import { JustifiedColumn, Spacer } from "components/util";
+import renderQueue from "util/renderQueue";
+
 import RouteDiagram from "components/routeDiagram/routeDiagramContainer";
 import Timetable from "components/timetable/timetableContainer";
 
@@ -14,35 +16,86 @@ import styles from "./stopPoster.css";
 
 // eslint-disable-next-line react/prefer-stateless-function
 class StopPoster extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasRoutesOnTop: false,
+        };
+    }
+
+    componentWillMount() {
+        renderQueue.add(this);
+    }
+
+    componentDidMount() {
+        this.handleLayout();
+    }
+
+    componentDidUpdate() {
+        this.handleLayout();
+    }
+
+    hasOverflow() {
+        return (this.content.scrollHeight > this.content.clientHeight) ||
+               (this.content.scrollHeight > this.content.clientHeight);
+    }
+
+    async handleLayout() {
+        await (() => new Promise(resolve => renderQueue.onEmpty(resolve, { ignore: this })))();
+
+        if (!this.hasOverflow()) {
+            renderQueue.remove(this, { success: true });
+            return;
+        }
+
+        if (!this.state.hasRoutesOnTop) {
+            this.setState({ hasRoutesOnTop: true });
+            return;
+        }
+
+        renderQueue.remove(this, { success: false });
+    }
+
     render() {
         return (
             <div className={styles.root}>
                 <JustifiedColumn>
                     <Header stopId={this.props.stopId}/>
 
-                    <div className={styles.content}>
-                        <div>
-                            <Routes stopId={this.props.stopId} date={this.props.date}/>
-                            <div className={styles.title}>Pysäkkiaikataulu</div>
-                            <Timetable
-                                stopId={this.props.stopId}
-                                date={this.props.date}
-                                isSummerTimetable={this.props.isSummerTimetable}
-                                dateBegin={this.props.dateBegin}
-                                dateEnd={this.props.dateEnd}
-                            />
-                        </div>
+                    <div className={styles.content} ref={(ref) => { this.content = ref; }}>
+                        {this.state.hasRoutesOnTop &&
+                            <span>
+                                <Routes stopId={this.props.stopId} date={this.props.date}/>
+                                <Spacer height={50}/>
+                            </span>
+                        }
+                        <div className={styles.columns}>
+                            <div className={styles.left}>
+                                {!this.state.hasRoutesOnTop &&
+                                    <span>
+                                        <Routes stopId={this.props.stopId} date={this.props.date}/>
+                                        <div className={styles.title}>Pysäkkiaikataulu</div>
+                                    </span>
+                                }
+                                <Timetable
+                                    stopId={this.props.stopId}
+                                    date={this.props.date}
+                                    isSummerTimetable={this.props.isSummerTimetable}
+                                    dateBegin={this.props.dateBegin}
+                                    dateEnd={this.props.dateEnd}
+                                />
+                            </div>
 
-                        <Spacer width={50}/>
+                            <Spacer width={50}/>
 
-                        <div>
-                            <Map stopId={this.props.stopId} date={this.props.date}/>
-                            <div className={styles.title}>Linjojen reitit</div>
-                            <RouteDiagram stopId={this.props.stopId} date={this.props.date}/>
+                            <div>
+                                <Map stopId={this.props.stopId} date={this.props.date}/>
+                                <div className={styles.title}>Linjojen reitit</div>
+                                <RouteDiagram stopId={this.props.stopId} date={this.props.date}/>
+                            </div>
                         </div>
                     </div>
 
-                    <FlexSpacer/>
                     <Footer/>
                 </JustifiedColumn>
             </div>
