a6ff1de003c62be23a0b41e6c815092fc52c3aab
Antti Silventoinen <antti.silventoinen@iki.fi>
2017-05-17 17:55:42 +0300

    Add QR code component to footer

--- src/components/stopPoster/footer.js
+++ src/components/stopPoster/footer.js
@@ -1,10 +1,14 @@
 import React from "react";
+import QrCode from "components/qrCode";
 import { Image } from "components/util";
 
 import footerIcon from "icons/footer.svg";
 
 const Footer = () => (
-    <div>
+    <div style={{ position: "relative" }}>
+        <div style={{ position: "absolute", left: 1093, top: 237 }}>
+            <QrCode url="http://www.hsl.fi/"/>
+        </div>
         <Image src={footerIcon}/>
     </div>
 );
