0fc0d89285051dadb8fbffeadeed277a37b446f3
Antti Silventoinen <antti.silventoinen@iki.fi>
2017-06-08 16:37:56 +0300

    Update destination styles

--- src/components/routeDiagram/destinations.js
+++ src/components/routeDiagram/destinations.js
@@ -1,22 +1,30 @@
 import React from "react";
+import { Row, Column, Spacer } from "components/util";
 import routeCompare from "util/routeCompare";
 
 import styles from "./destinations.css";
 
-const Destinations = props => (
-    <div>
-        {props.destinations &&
-            <div className={styles.destinations}>
-                {props.destinations.sort(routeCompare).map((route, index) =>
-                    <div key={index} className={styles.destination}>
-                        <div className={styles.routeId}>{route.routeId}</div>
+const Destinations = (props) => {
+    if (!props.destinations) {
+        return null;
+    }
+
+    const destinations = props.destinations.sort(routeCompare);
+
+    return (
+        <div className={styles.destinations}>
+            {destinations.map((route, index) => (
+                <Row key={index}>
+                    <div className={styles.routeId}>{route.routeId}</div>
+                    <Spacer width={5}/>
+                    <Column>
                         <div className={styles.title}>{route.title}</div>
                         <div className={styles.subtitle}>{route.subtitle}</div>
-                    </div>
-                )}
-            </div>
-        }
-    </div>
-);
+                    </Column>
+                </Row>
+            ))}
+        </div>
+    );
+};
 
 export default Destinations;
