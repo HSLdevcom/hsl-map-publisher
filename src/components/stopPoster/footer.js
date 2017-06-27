1eb6352d1e180d5238da74da4232e1887ff6b9fb
Antti Silventoinen <antti.silventoinen@iki.fi>
2017-06-27 18:58:57 +0300

    Add footer and feedback codes for tram stops

--- src/components/stopPoster/footer.js
+++ src/components/stopPoster/footer.js
@@ -3,21 +3,47 @@ import PropTypes from "prop-types";
 import QrCode from "components/qrCode";
 import { Image } from "components/util";
 
+import feedbackCodes from "data/feedbackCodes.json";
+
 import footerIcon from "icons/footer.svg";
+import tramFooterIcon from "icons/footer_tram.svg";
 import trunkRouteFooterIcon from "icons/footer_trunk.svg";
 
 import styles from "./footer.css";
 
 const Footer = (props) => {
-    const feedbackUrl = `hsl.fi/pysakit/${props.shortId.replace(" ", "")}`;
+    const stopInfoUrl = `hsl.fi/pysakit/${props.shortId.replace(" ", "")}`;
+    const feedbackUrl = `hsl.fi/fixit/${props.shortId.substring(3)}`;
+    // Feedback for tram stops
+    const hasFeedbackCode = props.shortId.startsWith("H 0") &&
+                           feedbackCodes.some(({ code }) => code === props.shortId.substring(3));
+
+    let src;
+    if (hasFeedbackCode) {
+        src = tramFooterIcon;
+    } else {
+        src = props.isTrunkStop ? trunkRouteFooterIcon : footerIcon;
+    }
 
     return (
         <div style={{ position: "relative" }}>
-            <Image src={props.isTrunkStop ? trunkRouteFooterIcon : footerIcon}/>
-            <div className={styles.shortCode}>
-                {feedbackUrl}
-            </div>
-            <QrCode url={`http://${feedbackUrl}`} className={styles.qrCode}/>
+            <Image src={src}/>
+
+            {!hasFeedbackCode &&
+                <span>
+                    <div className={styles.url}>{stopInfoUrl}</div>
+                    <QrCode className={styles.qrCode} url={`http://${stopInfoUrl}`}/>
+                </span>
+            }
+
+            {hasFeedbackCode &&
+                <span>
+                    <div className={styles.urlTram}>{stopInfoUrl}</div>
+                    <QrCode className={styles.qrCodeTram} url={`http://${stopInfoUrl}`}/>
+                    <div className={styles.urlFeedback}>{feedbackUrl}</div>
+                    <QrCode className={styles.qrCodeFeedback} url={`http://${feedbackUrl}`}/>
+                </span>
+            }
         </div>
     );
 };
