c9bba22b8e277fb122e6ee2979fd1aa2b9cb103e
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-11-08 06:27:58 +0200

    Align zone letters and extract the zone icon into its own component.

--- src/components/stopPoster/header.js
+++ src/components/stopPoster/header.js
@@ -4,6 +4,7 @@ import classNames from 'classnames';
 import { JustifiedRow, CenteringColumn } from 'components/util';
 
 import styles from './header.css';
+import ZoneIcon from './zoneIcon';
 
 const Group = props => <div style={{ marginLeft: 15, marginRight: 15 }}>{props.children}</div>;
 
@@ -41,33 +42,28 @@ Subtitle.propTypes = {
 
 // TODO: Fix alignment of zone letter for each letter.
 
-const Header = props => {
-  const zone = props.stopZone;
-  return (
-    <JustifiedRow style={{ margin: '0 10px' }}>
-      <Group>
-        <Title>{props.nameFi}</Title>
-        {props.nameSe && <Subtitle>{props.nameSe}</Subtitle>}
-      </Group>
-      <CenteringColumn>
-        <Title small>Pysäkkinumero</Title>
-        <Subtitle small>Hållplatsnummer</Subtitle>
-        <div className={styles.stop}>{props.shortId.replace(' ', '')}</div>
-      </CenteringColumn>
-      {zone && (
-        <div className={styles.stopZoneColumn}>
-          <div className={styles.zoneHeading}>
-            <Title>Vyöhyke</Title>
-            <Subtitle>Zon/Zone</Subtitle>
-          </div>
-          <div className={styles.zone}>
-            <span className={styles.zoneLetter}>{zone}</span>
-          </div>
+const Header = props => (
+  <JustifiedRow style={{ margin: '0 10px' }}>
+    <Group>
+      <Title>{props.nameFi}</Title>
+      {props.nameSe && <Subtitle>{props.nameSe}</Subtitle>}
+    </Group>
+    <CenteringColumn>
+      <Title small>Pysäkkinumero</Title>
+      <Subtitle small>Hållplatsnummer</Subtitle>
+      <div className={styles.stop}>{props.shortId.replace(' ', '')}</div>
+    </CenteringColumn>
+    {props.stopZone && (
+      <div className={styles.stopZoneColumn}>
+        <div className={styles.zoneHeading}>
+          <Title>Vyöhyke</Title>
+          <Subtitle>Zon/Zone</Subtitle>
         </div>
-      )}
-    </JustifiedRow>
-  );
-};
+        <ZoneIcon zone={props.stopZone} />
+      </div>
+    )}
+  </JustifiedRow>
+);
 
 Header.defaultProps = {
   nameSe: null,
---
+++ src/components/stopPoster/zoneIcon.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './header.css';
+
+function ZoneIcon({ zone }) {
+  // All letters sit a bit differently in the circle. Oh well.
+  const zoneLetterStyle = {
+    transform:
+      zone === 'B'
+        ? 'translate(calc(-50% + 3px), calc(-50% + 6px))'
+        : zone === 'C'
+          ? 'translate(calc(-50% - 4px), calc(-50% + 7px))'
+          : zone === 'D'
+            ? 'translate(calc(-50% + 6px), calc(-50% + 7px))'
+            : 'translate(-50%, -50%)', // No px adjustments for zone A and the "else" case.
+    fontSize: '140px',
+  };
+
+  return (
+    <div className={styles.zone}>
+      <span className={styles.zoneLetter} style={zoneLetterStyle}>
+        {zone}
+      </span>
+    </div>
+  );
+}
+
+ZoneIcon.propTypes = {
+  zone: PropTypes.string.isRequired,
+};
+
+export default ZoneIcon;
