5072542cc5d220082a50e334ac211d052785afaa
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-12-05 05:40:22 +0200

    Show routes and stop zone in standalone timetables. (#193)

--- src/components/timetable/timetableContainer.js
+++ src/components/timetable/timetableContainer.js
@@ -81,6 +81,8 @@ const timetableQuery = gql`
       nameFi
       nameSe
       shortId
+      stopId
+      stopZone
       siblings {
         nodes {
           routeSegments: routeSegmentsForDate(date: $date) {
@@ -228,6 +230,7 @@ const propsMapper = mapProps(props => {
     notes,
     dateBegin,
     dateEnd,
+    date: props.date,
     isSummerTimetable: props.isSummerTimetable,
     showValidityPeriod: props.showValidityPeriod,
     showNotes: props.showNotes,
@@ -236,6 +239,8 @@ const propsMapper = mapProps(props => {
     stopNameFi: props.data.stop.nameFi,
     stopNameSe: props.data.stop.nameSe,
     stopShortId: props.data.stop.shortId,
+    stopId: props.data.stop.stopId,
+    stopZone: props.data.stop.stopZone,
     printableAsA4: props.printTimetablesAsA4,
     greyscale: props.printTimetablesAsGreyscale,
     standalone: props.standalone,
