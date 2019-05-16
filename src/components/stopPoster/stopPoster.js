33db1082d38e6c02103f87630483eac5a36c93bd
Anton <anton.jyrkiainen@helsinki.fi>
2019-05-16 16:39:23 +0300

    revert previous changes

--- src/components/stopPoster/stopPoster.js
+++ src/components/stopPoster/stopPoster.js
@@ -258,18 +258,16 @@ class StopPoster extends Component {
       return;
     }
 
-    // If there is no layout overflow and the map is not rendered, try rendering the map again.
-    if (!this.state.shouldRenderMap && !this.state.triedRenderingMap) {
-      this.setState({ shouldRenderMap: true });
+    if (this.state.template && this.state.removedAds.length > 0) {
+      this.setState({
+        adsPhase: true,
+      });
       return;
     }
 
-    if (this.state.template && this.state.removedAds.length > 0) {
-      // We don't want to render map multiple times when doing the ads.
-      this.setState({
-        adsPhase: true,
-        shouldRenderMap: false,
-      });
+    // If there is no layout overflow and the map is not rendered, try rendering the map again.
+    if (!this.state.shouldRenderMap && !this.state.triedRenderingMap) {
+      this.setState({ shouldRenderMap: true });
       return;
     }
 
