96c4d0a28288926e893ade5b7ce7b8fe884a1579
Anton <anton.jyrkiainen@helsinki.fi>
2019-08-19 16:38:41 +0300

    initial logic for downloading

--- seeds/default_template.js
+++
@@ -1,7 +0,0 @@
-const createEmptyTemplate = require('../scripts/util/createEmptyTemplate');
-
-exports.seed = function(knex) {
-  return knex('template')
-    .del()
-    .then(() => knex('template').insert([createEmptyTemplate('Default', 'default')]));
-};
--- src/components/labelPlacement/optimizePositions.worker.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-restricted-globals */
-
-import optimizePositions from './optimizePositions';
-
-self.addEventListener('message', event => {
-  const { positions, boundingBox } = event.data;
-  const optimizedPositions = optimizePositions(positions, boundingBox);
-  self.postMessage(optimizedPositions);
-});
