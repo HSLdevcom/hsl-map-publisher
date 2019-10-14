2b61f35c18ad4eb490c3f183d897f55c1d4b3730
Anton <anton.jyrkiainen@helsinki.fi>
2019-10-14 09:54:14 +0300

    eslint fix

--- scripts/auth/authEndpoints.js
+++ scripts/auth/authEndpoints.js
@@ -1,83 +1,122 @@
+const { get, last, clone } = require('lodash');
 const AuthService = require('./authService');
+
+// TODO: Envify
+const allowedDomains = ['cgi.com', 'hsl.fi'];
+const allowedGroup = 'Karttajulkaisin';
+
+const hasAllowedGroup = async userInfo => {
+  const groups = get(userInfo, 'groups');
+  const domain = last(userInfo.email.toLowerCase().split('@')) || '';
+
+  if (!allowedDomains.includes(domain)) {
+    console.log(`User does not have allowed group in hsl-id. Logging out.`);
+    return false;
+  }
+
+  if (allowedDomains.includes(domain) && !groups.includes(allowedGroup)) {
+    groups.push(allowedGroup);
+    const response = await AuthService.setGroup(userInfo.userId, groups);
+  }
+  return true;
+};
 
 const authorize = async (req, res, session) => {
   const authRequest = req.body;
+  const modifiedSession = clone(session);
 
-  if (!Boolean(authRequest.code)) {
-    console.log('No authorization code')
+  if (!authRequest.code) {
+    console.log('No authorization code');
     return {
       body: {
         isOk: false,
       },
       status: 401,
-    }
+    };
   }
-  const tokenResponse = await AuthService.requestAccessToken(authRequest.code)
+  const tokenResponse = await AuthService.requestAccessToken(authRequest.code);
 
   if (session && tokenResponse.access_token) {
-    session.accessToken = tokenResponse.access_token
-    const userInfo = await AuthService.requestUserInfo(session.accessToken)
-    session.email = userInfo.email
-    session.groups = userInfo.groups
+    modifiedSession.accessToken = tokenResponse.access_token;
+    const userInfo = await AuthService.requestUserInfo(modifiedSession.accessToken);
+    const isAllowed = await hasAllowedGroup(userInfo);
+    if (!isAllowed) {
+      return {
+        status: 401,
+        body: {
+          isOk: false,
+          message: 'No allowed group.',
+        },
+      };
+    }
+
+    modifiedSession.email = userInfo.email;
+    modifiedSession.groups = userInfo.groups;
 
     const response = {
       isOk: true,
       email: userInfo.email,
-    }
+    };
 
     return {
       status: 200,
       body: response,
-      session,
-    };
-  } else {
-    console.log('No access token: ', tokenResponse)
-    const response = {
-      isOk: false,
-    }
-    return {
-      status: 401,
-      body: response,
+      modifiedSession,
     };
   }
-}
+  console.log('No access token: ', tokenResponse);
+  const response = {
+    isOk: false,
+  };
+  return {
+    status: 401,
+    body: response,
+  };
+};
 
 const checkExistingSession = async (req, res, session) => {
   if (session && session.accessToken) {
+    const isAllowed = await hasAllowedGroup(session);
+    if (!isAllowed) {
+      await AuthService.logoutFromIdentityProvider(session.accessToken);
+      return {
+        status: 200,
+      };
+    }
+
     const response = {
       isOk: true,
       email: session.email,
-    }
+    };
     return {
       status: 200,
       body: response,
     };
   }
-  console.log('No existing session')
+  console.log('No existing session');
   const response = {
     isOk: false,
-  }
+  };
   return {
     status: 200,
     body: response,
   };
-}
+};
 
 const logout = async (req, res, session) => {
   if (session && session.accessToken) {
-    await AuthService.logoutFromIdentityProvider(session.accessToken)
+    await AuthService.logoutFromIdentityProvider(session.accessToken);
     return {
       status: 200,
     };
-  } else {
-    return {
-      status: 401,
-    };
   }
-}
+  return {
+    status: 401,
+  };
+};
 
 module.exports = {
   authorize,
   checkExistingSession,
   logout,
-}
+};
--- scripts/auth/authService.js
+++ scripts/auth/authService.js
@@ -1,6 +1,14 @@
 const nodeFetch = require('node-fetch');
 
-const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, LOGIN_PROVIDER_URI } = process.env;
+const {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI,
+  LOGIN_PROVIDER_URI,
+  API_CLIENT_ID,
+  API_CLIENT_SECRET,
+} = process.env;
+const authHash = Buffer.from(`${API_CLIENT_ID}:${API_CLIENT_SECRET}`).toString('base64');
 
 const requestAccessToken = async code => {
   const url = `${LOGIN_PROVIDER_URI}/openid/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=authorization_code&code=${code}&redirect_uri=${REDIRECT_URI}`;
@@ -40,4 +48,21 @@ const logoutFromIdentityProvider = async accessToken => {
   });
 };
 
-module.exports = { requestAccessToken, requestUserInfo, logoutFromIdentityProvider };
+const setGroup = async (userId, groups) => {
+  const url = `${LOGIN_PROVIDER_URI}/api/rest/v1/user/${userId}`;
+  const response = await nodeFetch(url, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${authHash}`,
+    },
+    body: JSON.stringify({
+      memberOf: groups,
+    }),
+  });
+
+  return response.json();
+};
+
+module.exports = { requestAccessToken, requestUserInfo, logoutFromIdentityProvider, setGroup };
