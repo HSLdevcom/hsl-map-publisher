8af6e89d3679b05e0f13777973bd53d230f14cd4
Anton <anton.jyrkiainen@helsinki.fi>
2019-11-13 10:07:40 +0200

    server url fixed

--- src/util/map.js
+++ src/util/map.js
@@ -9,7 +9,7 @@ const scale = 5;
  */
 // eslint-disable-next-line import/prefer-default-export
 export async function fetchMap(mapOptions, mapStyle) {
-  const serverUrl = 'http://localhost:8000';
+  const serverUrl = process.env.GENERATE_API_URL || 'https://kartat.hsl.fi';
   window.serverLog(`Generating the map with ${serverUrl}`);
 
   const options = {
