b142fc2130021533cebdd379724c26193aa1805f
Anton <anton.jyrkiainen@helsinki.fi>
2020-04-29 20:28:02 +0300

    routes that belong to stop that has terminalId shown in routediagram

--- src/components/routeDiagram/path.js
+++ src/components/routeDiagram/path.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Destinations from './destinations';
@@ -9,49 +9,92 @@ import Zone from './zone';
 import styles from './path.css';
 
 // Must match width and radius values in CSS
-const PATH_WIDTH = 217;
+const PATH_WIDTH = 224;
+const PATH_WIDTH_WIDER = 258;
 const LINE_RADIUS = 10;
 
-function getWidth(nodes, isRoot = true) {
-  let width = 0;
-  nodes.forEach((node, index) => {
-    if (!node.children || (isRoot && index === nodes.length - 1)) {
-      width += PATH_WIDTH;
-    } else {
-      width += getWidth(node.children, false);
-    }
-  });
-  return isRoot ? width - PATH_WIDTH - LINE_RADIUS : width;
-}
+class Path extends Component {
+  hasTerminalId = branch => {
+    let foundTerminalId = false;
+    branch.items.forEach(item => {
+      if (item.terminalId && item.routeSegments.nodes.length > 1) {
+        foundTerminalId = true;
+      }
+    });
+    return foundTerminalId;
+  };
 
-function isLastStop(item) {
-  return !!item.destinations && item.destinations.length > 0;
-}
+  getWidth = (nodes, isRoot = true) => {
+    let width = 0;
+    nodes.forEach((node, index) => {
+      if (!node.children || (isRoot && index === nodes.length - 1)) {
+        if (this.hasTerminalId(node)) {
+          width += PATH_WIDTH_WIDER;
+        } else {
+          width += PATH_WIDTH;
+        }
+      } else {
+        width += this.getWidth(node.children, false);
+      }
+    });
 
-const Path = props => (
-  <div className={styles.root}>
-    <div className={styles.header} />
-    {props.items.map((item, index) => (
-      <div key={index}>
-        {item.type === 'stop' && <Stop {...item} isLast={isLastStop(item)} />}
-        {item.type === 'gap' && <Gap />}
-        {item.type === 'zone' && <Zone {...item} />}
-        {item.destinations && <Destinations destinations={item.destinations} />}
+    return isRoot
+      ? width -
+          (this.hasTerminalId(nodes[nodes.length - 1]) ? PATH_WIDTH_WIDER : PATH_WIDTH) -
+          LINE_RADIUS
+      : width;
+  };
+
+  getDestinationRouteIds = items => {
+    const destinationRouteIds = [];
+    items.forEach(item => {
+      if (item.destinations) {
+        item.destinations.forEach(destination => destinationRouteIds.push(destination.routeId));
+      }
+    });
+    return destinationRouteIds;
+  };
+
+  isLastStop = item => !!item.destinations && item.destinations.length > 0;
+
+  isFirstStop = (item, index) => !!item.destinations && item.destinations.length > 0 && index === 0;
+
+  render() {
+    const destinationRouteIds = this.getDestinationRouteIds(this.props.items);
+    const hasTerminalId = this.hasTerminalId(this.props);
+    return (
+      <div className={styles.root}>
+        <div className={styles.header} />
+        {this.props.items.map((item, index) => (
+          <div key={index}>
+            {item.type === 'stop' && (
+              <Stop
+                {...item}
+                isFirst={this.isFirstStop(item, index)}
+                isLast={this.isLastStop(item)}
+                destinationRouteIds={destinationRouteIds}
+                hasTerminalId={hasTerminalId}
+              />
+            )}
+            {item.type === 'gap' && <Gap />}
+            {item.type === 'zone' && <Zone {...item} />}
+            {item.destinations && <Destinations destinations={item.destinations} />}
+          </div>
+        ))}
+        {this.props.children && (
+          <div>
+            <div className={styles.footer} style={{ width: this.getWidth(this.props.children) }} />
+            <div className={styles.children}>
+              {this.props.children.map((branch, index) => (
+                <Path key={index} {...branch} />
+              ))}
+            </div>
+          </div>
+        )}
       </div>
-    ))}
-
-    {props.children && (
-      <div>
-        <div className={styles.footer} style={{ width: getWidth(props.children) }} />
-        <div className={styles.children}>
-          {props.children.map((branch, index) => (
-            <Path key={index} {...branch} />
-          ))}
-        </div>
-      </div>
-    )}
-  </div>
-);
+    );
+  }
+}
 
 Path.defaultProps = {
   children: null,
--- src/components/routeDiagram/stop.js
+++ src/components/routeDiagram/stop.js
@@ -1,54 +1,121 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { InlineSVG } from 'components/util';
-import { iconsByMode } from 'util/domain';
-
+import { iconsByMode, trimRouteId } from 'util/domain';
+import { Column, InlineSVG } from 'components/util';
 import styles from './stop.css';
 
 const metroRegexp = / ?\(M\)$/;
+const MAX_TERMINAL_ROUTE_DIVS = 25;
 
-const Stop = props => {
-  const modes = new Set();
-  if (metroRegexp.test(props.nameFi)) modes.add('SUBWAY');
-  props.transferModes.forEach(mode => modes.add(mode));
+class Stop extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  return (
-    <div className={styles.root}>
-      <div className={styles.left} />
-      <div className={styles.separator}>
-        <div className={styles.separatorTop} />
-        <div className={props.isLast ? styles.separatorLastStop : styles.separatorStop} />
-        <div
-          className={styles.separatorBottom}
-          style={{ visibility: props.isLast ? 'hidden' : 'visible' }}
-        />
+  componentDidMount() {
+    const height = this.divElement.clientHeight - 10;
+    this.setState({ height });
+  }
+
+  extractNumbers = routeId => {
+    const matches = routeId.match(/(\d+)/);
+    return matches[0];
+  };
+
+  getTerminalAreaRoutes = props => {
+    const routes = [];
+    props.routeSegments.nodes.forEach(segment => {
+      const routeId = trimRouteId(segment.routeId);
+      const routeIdNumber = this.extractNumbers(routeId);
+      if (!props.destinationRouteIds.includes(routeId) && segment.hasRegularDayDepartures)
+        routes.push({ routeId, routeIdNumber });
+    });
+    const sortedRoutes = routes.sort((a, b) => a.routeIdNumber - b.routeIdNumber);
+    return sortedRoutes.map(route => route.routeId);
+  };
+
+  render() {
+    const modes = new Set();
+    if (metroRegexp.test(this.props.nameFi)) modes.add('SUBWAY');
+    this.props.transferModes.forEach(mode => modes.add(mode));
+
+    const terminalAreaRoutes = this.getTerminalAreaRoutes(this.props);
+    if (terminalAreaRoutes.length > MAX_TERMINAL_ROUTE_DIVS) {
+      terminalAreaRoutes.length = MAX_TERMINAL_ROUTE_DIVS;
+      terminalAreaRoutes.push('...');
+    }
+
+    const terminalAreaRouteDivs = terminalAreaRoutes.map((route, index) => (
+      <div key={index} className={styles.routeContainer}>
+        {route}
       </div>
-      <div className={styles.right}>
-        <div>
-          <div className={styles.title}>{props.nameFi.replace(metroRegexp, '')}</div>
-          <div className={styles.subtitle}>
-            {props.nameSe && props.nameSe.replace(metroRegexp, '')}
+    ));
+
+    const showTerminalAreaRoutesContainer =
+      terminalAreaRouteDivs.length > 0 && this.props.terminalId;
+
+    return (
+      <div
+        ref={divElement => {
+          this.divElement = divElement;
+        }}
+        className={styles.root}>
+        <div className={styles.left} />
+        <div className={styles.separator}>
+          <div
+            style={{
+              height: this.state.height ? `${this.state.height / 2}px` : '12px',
+              borderLeft: 'var(--line-border)',
+            }}
+          />
+          <div className={this.props.isLast ? styles.separatorLastStop : styles.separatorStop} />
+          <div
+            style={{
+              height: this.state.height ? `${this.state.height / 2}px` : '12px',
+              borderLeft: 'var(--line-border)',
+              visibility: this.props.isLast ? 'hidden' : 'visible',
+            }}
+          />
+        </div>
+        <div className={this.props.hasTerminalId ? styles.rightWide : styles.right}>
+          <div className={styles.titleContainer}>
+            <Column>
+              <div className={styles.title}>{this.props.nameFi.replace(metroRegexp, '')}</div>{' '}
+              <div className={styles.subtitle}>
+                {this.props.nameSe && this.props.nameSe.replace(metroRegexp, '')}
+              </div>
+            </Column>
           </div>
-        </div>
-        <div className={styles.iconContainer}>
-          {Array.from(modes).map((mode, index) => (
-            <InlineSVG key={index} className={styles.icon} src={iconsByMode[mode]} />
-          ))}
+          <div className={styles.iconContainer}>
+            {Array.from(modes).map((mode, index) => (
+              <InlineSVG key={index} className={styles.icon} src={iconsByMode[mode]} />
+            ))}
+          </div>
+          {showTerminalAreaRoutesContainer && (
+            <div className={styles.terminalAreaRoutesContainer}>{terminalAreaRouteDivs}</div>
+          )}
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Stop.defaultProps = {
   nameSe: null,
+  destinationRouteIds: [],
+  terminalId: null,
 };
 
 Stop.propTypes = {
+  terminalId: PropTypes.string,
+  hasTerminalId: PropTypes.bool.isRequired,
   nameFi: PropTypes.string.isRequired,
   nameSe: PropTypes.string,
   isLast: PropTypes.bool.isRequired,
+  isFirst: PropTypes.bool.isRequired,
+  destinationRouteIds: PropTypes.array,
   transferModes: PropTypes.arrayOf(PropTypes.oneOf(['BUS', 'TRAM', 'FERRY', 'RAIL', 'SUBWAY']))
     .isRequired,
 };
