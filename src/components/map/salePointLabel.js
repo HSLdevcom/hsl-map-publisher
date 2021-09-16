e760374c47721e9afc04163a7b2b30b8e8516bbc
Juho Hänninen <juho.hanninen@cgi.com>
2021-09-16 16:15:36 +0300

    Add icon to the ticket sales point label

--- src/components/map/salePointLabel.js
+++ src/components/map/salePointLabel.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { InlineSVG } from 'components/util';
+import iconTicketSales from 'icons/icon-tickets-sales-point.svg';
+import iconTicketMachine from 'icons/icon-ticket-machine.svg';
+
 import styles from './stopLabel.css';
 
 const SalePointLabel = props => {
   // Round the distance to the next 50 meter
   const roundedDistance = Math.ceil(props.distance / 50) * 50;
+
+  const ticketIcon =
+    props.type.toLowerCase() === 'myyntipiste' ? iconTicketSales : iconTicketMachine;
+
   return (
     <div className={styles.label}>
-      <div className={styles.title}>{props.title}</div>
-      <div className={styles.subtitle}>{props.address}</div>
+      <div className={styles.flexContainer}>
+        <div className={styles.comma}>
+          <InlineSVG src={ticketIcon} />
+        </div>
+        <div>
+          <div className={styles.title}>{props.title}</div>
+          <div className={styles.subtitle}>{props.address}</div>
+        </div>
+      </div>
       <div className={styles.subtitle}>{`(${roundedDistance} metriä / meter / meters)`}</div>
       <div className={styles.content}>
         <div className={styles.flexContainer}>
