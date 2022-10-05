4ae3d151a3419f6cd385c3ad34290805a97fdd62
e-halinen <erkki-eemeli.halinen@cgi.com>
2022-10-05 15:37:14 +0300

    MM-418: use joreDate-parameter with generation date

--- src/components/map/mapImageContainer.js
+++ src/components/map/mapImageContainer.js
@@ -6,42 +6,21 @@ import hslMapStyle from 'hsl-map-style';
 import { fetchMap } from 'util/map';
 import promiseWrapper from 'util/promiseWrapper';
 import MapImage from './mapImage';
-import { mapValues } from 'lodash';
 
 const propsMapper = mapProps(({ options, components, date, extraLayers }) => {
   const mapStyle = hslMapStyle.generateStyle({
     components,
+    joreDate: date,
   });
-
-  const sources = mapValues(mapStyle.sources, (value, key) => {
-    // eslint-disable-next-line no-param-reassign
-    value.url += `?date=${date}`;
-    return value;
-  });
-
-  mapStyle.sources = sources;
-
-  // Remove source containing bus routes (rail and subway routes have separate sources)
-  if (components.routes && components.routes.enabled && components.routes.hideBusRoutes) {
-    mapStyle.sources.routes = {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: [],
-      },
-    };
-  }
 
   if (extraLayers) {
     mapStyle.layers = [...mapStyle.layers, ...extraLayers];
   }
+
   return { src: fetchMap(options, mapStyle) };
 });
 
-const hoc = compose(
-  propsMapper,
-  promiseWrapper('src'),
-);
+const hoc = compose(propsMapper, promiseWrapper('src'));
 
 const MapImageContainer = hoc(MapImage);
 
@@ -63,7 +42,6 @@ MapImageContainer.propTypes = {
   components: PropTypes.objectOf(
     PropTypes.shape({
       enabled: PropTypes.bool.isRequired,
-      hideBusRoutes: PropTypes.bool,
     }),
   ).isRequired,
   date: PropTypes.string,
