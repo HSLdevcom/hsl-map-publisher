ec906c137ed321b9857dfe67711aa7fd7ef3cfab
Juho Hänninen <juho.hanninen@cgi.com>
2023-01-03 09:53:40 +0200

    Separate worker and generator logic

---
+++ scripts/generator.js
@@ -0,0 +1,174 @@
+const fs = require('fs-extra');
+const path = require('path');
+const puppeteer = require('puppeteer');
+const qs = require('qs');
+const log = require('./util/log');
+const { uploadPosterToCloud } = require('./cloudService');
+
+const { addEvent, updatePoster } = require('./store');
+
+const { AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_KEY } = require('../constants');
+
+const CLIENT_URL = 'http://localhost:5000';
+const RENDER_TIMEOUT = 10 * 60 * 1000;
+const MAX_RENDER_ATTEMPTS = 3;
+const SCALE = 96 / 72;
+
+let browser = null;
+const cwd = process.cwd();
+
+const pdfOutputDir = path.join(cwd, 'output');
+
+const pdfPath = id => path.join(pdfOutputDir, `${id}.pdf`);
+
+async function initialize() {
+  browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-web-security'],
+  });
+  browser.on('disconnected', () => {
+    browser = null;
+  });
+}
+
+/**
+ * Renders component to PDF file
+ * @returns {Promise}
+ */
+async function renderComponent(options) {
+  const { id, component, template, props, onInfo, onError } = options;
+
+  const page = await browser.newPage();
+
+  await page.exposeFunction('serverLog', log);
+
+  page.on('error', error => {
+    page.close();
+    browser.close();
+    onError(error);
+  });
+
+  page.on('console', ({ type, text }) => {
+    if (['error', 'warning', 'log'].includes(type)) {
+      onInfo(`Console(${type}): ${text}`);
+    }
+  });
+
+  const encodedProps = qs.stringify({ component, props, template });
+  const pageUrl = `${CLIENT_URL}/?${encodedProps}`;
+
+  console.log(`Opening ${pageUrl} in Puppeteer.`);
+
+  await page.goto(pageUrl, {
+    timeout: RENDER_TIMEOUT,
+  });
+
+  const { error = null, width, height } = await page.evaluate(
+    () =>
+      new Promise(resolve => {
+        window.callPhantom = opts => resolve(opts);
+      }),
+  );
+
+  if (error) {
+    throw new Error(error);
+  }
+
+  await page.emulateMediaType('screen');
+
+  let printOptions = {};
+  if (props.printTimetablesAsA4) {
+    printOptions = {
+      printBackground: true,
+      format: 'A4',
+      margin: 0,
+    };
+  } else {
+    printOptions = {
+      printBackground: true,
+      width: width * SCALE,
+      height: height * SCALE,
+      pageRanges: '1',
+      scale: SCALE,
+    };
+  }
+
+  const contents = await page.pdf(printOptions);
+
+  const pdfFilePath = pdfPath(id);
+  await fs.outputFile(pdfFilePath, contents);
+  await page.close();
+
+  const posterUploaded = await uploadPosterToCloud(pdfFilePath);
+  return posterUploaded;
+}
+
+async function renderComponentRetry(options) {
+  const { onInfo, onError } = options;
+
+  for (let i = 0; i < MAX_RENDER_ATTEMPTS; i++) {
+    /* eslint-disable no-await-in-loop */
+    try {
+      onInfo(i > 0 ? 'Retrying' : 'Rendering');
+      if (!browser) {
+        onInfo('Creating new browser instance');
+        await initialize();
+      }
+      const timeout = new Promise((resolve, reject) =>
+        setTimeout(reject, RENDER_TIMEOUT, new Error('Render timeout')),
+      );
+
+      const posterUploaded = await Promise.race([renderComponent(options), timeout]);
+      const uploadFailed = !posterUploaded && AZURE_STORAGE_ACCOUNT && AZURE_STORAGE_KEY;
+
+      if (!uploadFailed) {
+        onInfo('Rendered successfully.');
+      } else {
+        const err = { message: 'Rendered successfully but uploading poster failed.', stack: '' };
+        throw err;
+      }
+
+      return { success: true, uploaded: !uploadFailed };
+    } catch (error) {
+      onError(error);
+    }
+    /* eslint-enable no-await-in-loop */
+  }
+
+  return { success: false };
+}
+
+async function generate(options) {
+  const { id } = options;
+
+  const onInfo = (message = 'No message.') => {
+    console.log(`${id}: ${message}`); // eslint-disable-line no-console
+    addEvent({
+      posterId: id,
+      type: 'INFO',
+      message,
+    });
+  };
+  const onError = error => {
+    console.error(`${id}: ${error.message} ${error.stack}`); // eslint-disable-line no-console
+    addEvent({
+      posterId: id,
+      type: 'ERROR',
+      message: error.message,
+    });
+  };
+
+  const { success, uploaded } = await renderComponentRetry({
+    ...options,
+    onInfo,
+    onError,
+  });
+
+  updatePoster({
+    id,
+    status: success && uploaded ? 'READY' : 'FAILED',
+  });
+}
+
+module.exports = {
+  generate,
+};
