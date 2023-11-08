19c6d84f04c496d5bd20f214649e91c74f002097
e-halinen <erkki-eemeli.halinen@cgi.com>
2023-11-08 20:42:18 +0200

    Fix StopPosterA3 generating error

--- src/components/a3stopPoster/a3header.js
+++ src/components/a3stopPoster/a3header.js
@@ -120,7 +120,7 @@ class A3Header extends Component {
         {this.state.simpleRoutes && !this.state.hideDestinations && (
           <SimpleRoutes
             printAsA3
-            stopIds={[this.props.variables.stopId]}
+            stopId={this.props.variables.stopId}
             date={this.props.variables.date}
           />
         )}
