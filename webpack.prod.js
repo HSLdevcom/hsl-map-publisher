7927347116e6b7aa892817097d5ec7a5aa7e82ff
Juho Hänninen <hannisenjuho@gmail.com>
2024-05-20 13:31:39 +0300

    Use Apollo in prod mode on builds

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,16 @@
 const merge = require('webpack-merge');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const { DefinePlugin } = require('webpack');
 
 const webpackCommon = require('./webpack.common');
 
 module.exports = merge.smart(webpackCommon, {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [new ESLintPlugin({ cache: true, fix: true })],
+  plugins: [
+    new ESLintPlugin({ cache: true, fix: true }),
+    new DefinePlugin({
+      'globalThis.__DEV__': false,
+    }),
+  ],
 });
